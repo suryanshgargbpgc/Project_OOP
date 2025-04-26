212f702b5090fa77107c20c92c7735d465f0972c
Tanishq Agarwal <117887119+Tanishq2807@users.noreply.github.com>
2025-04-26 22:39:27 +0530

    Update User.java

--- src/com/pharmacy/model/User.java
+++ src/com/pharmacy/model/User.java
@@ -2,54 +2,39 @@ package com.pharmacy.model;
 
 import java.util.Date;
 
-/**
- * Base user class for the pharmacy system
- * Written by: Student
- * Date: 11/10/2023
- */
 public abstract class User {
-    // Public variables for easy access
     public String userID;
     public String userName;
     public String userEmail;
     public String userPhone;
     public String userAddress;
-    public Date userBirth;
-    
-    // Empty constructor
+
     public User() {
         this.userID = "";
         this.userName = "";
         this.userEmail = "";
         this.userPhone = "";
         this.userAddress = "";
-        this.userBirth = new Date();
     }
     
-    // Basic constructor
     public User(String id, String name, String email, String phone) {
         this.userID = id;
         this.userName = name;
         this.userEmail = email;
         this.userPhone = phone;
         this.userAddress = "";
-        this.userBirth = new Date();
     }
     
-    // Full constructor with all info
-    public User(String id, String name, String email, String phone, String address, Date birth) {
+    public User(String id, String name, String email, String phone, String address) {
         this.userID = id;
         this.userName = name;
         this.userEmail = email;
         this.userPhone = phone;
         this.userAddress = address;
-        this.userBirth = birth;
     }
     
-    // Each user type must implement this
     public abstract String getUserType();
     
-    // Basic get/set methods with simple names
     public String getUserId() {
         return userID;
     }
@@ -89,16 +74,7 @@ public abstract class User {
     public void setAddress(String address) {
         this.userAddress = address;
     }
-
-    public Date getBirthDate() {
-        return userBirth;
-    }
-
-    public void setBirthDate(Date birth) {
-        this.userBirth = birth;
-    }
     
-    // Print user information
     public String toString() {
         String output = "";
         output = output + "User Information:\n";
